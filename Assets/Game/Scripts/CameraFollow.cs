a96d2a38f002689c4e396906e3141f837b0589cb
r Star <starlove124760@gmail.com>
2023-02-04 04:10:40 +0800

    add mapBounds

--- Assets/Game/Scripts/CameraFollow.cs
+++ Assets/Game/Scripts/CameraFollow.cs
@@ -1,4 +1,8 @@
+#region
+
 using UnityEngine;
+
+#endregion
 
 namespace Game.Scripts
 {
@@ -6,16 +10,43 @@ namespace Game.Scripts
     {
     #region Private Variables
 
+        private float  xMin , xMax , yMin , yMax;
+        private float  camY , camX;
+        private float  camOrthsize;
+        private float  cameraRatio;
+        private Camera mainCam;
+
         [SerializeField]
         private Transform followTransform;
+
+        [SerializeField]
+        private BoxCollider2D mapBounds;
 
     #endregion
 
     #region Unity events
 
-        private void Update()
+        private void Start()
         {
-            transform.position = new Vector3(followTransform.position.x , transform.position.y , transform.position.z);
+            xMin        = mapBounds.bounds.min.x;
+            xMax        = mapBounds.bounds.max.x;
+            yMin        = mapBounds.bounds.min.y;
+            yMax        = mapBounds.bounds.max.y;
+            mainCam     = GetComponent<Camera>();
+            camOrthsize = mainCam.orthographicSize;
+            cameraRatio = (xMax + camOrthsize) / 2.0f;
+        }
+
+    #endregion
+
+    #region Private Methods
+
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            camY               = Mathf.Clamp(followTransform.position.y , yMin + camOrthsize , yMax - camOrthsize);
+            camX               = Mathf.Clamp(followTransform.position.x , xMin + cameraRatio , xMax - cameraRatio);
+            transform.position = new Vector3(camX , camY , transform.position.z);
         }
 
     #endregion
